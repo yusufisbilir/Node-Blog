8414a286fa5a60c04f4eba45b560cce167bc2627
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-24 23:15:44 +0300

    add data to database

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const { render } = require('ejs');
 const express = require('express');
 const mongoose = require('mongoose');
+const Blog = require('./models/blogs');
+
 const app = express();
 const port = 3000;
 
-const dbUrl = 'mongodb+srv://yusufisbilir:1234@cluster0.691wr.mongodb.net/nodeBlog?retryWrites=true&w=majority'
+const dbUrl = 'mongodb+srv://yusufisbilir:1234@nodejs-blog.691wr.mongodb.net/node-blog?retryWrites=true&w=majority';
 mongoose.connect(dbUrl,{useNewUrlParser:true, useUnifiedTopology:true})
     .then((result)=>{
         app.listen(port);
@@ -13,8 +15,21 @@ mongoose.connect(dbUrl,{useNewUrlParser:true, useUnifiedTopology:true})
     })
 
 app.use(express.static('public'));
-
 app.set('view engine','ejs');
+
+app.get('/add',(req,res)=>{
+    const blog = new Blog({
+        title:'new title',
+        short:'short description',
+        long:'writing'
+    });
+    
+    blog.save()
+        .then(result =>{
+            res.send(result);
+        })
+        .catch(err => {console.log(err)});
+});
 
 app.get('/', (req, res) => {
     res.render('index',{title:'Main Page'})
