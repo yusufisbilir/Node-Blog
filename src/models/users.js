9a8fa385e46ead890929e4ec62ad7098ba33f83d
Yusuf Emir Isbilir <yusufemirisbilir@gmail.com>
2021-03-25 21:57:20 +0300

    add user model password bcyrpt

--- src/models/users.js
+++ src/models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
     username:{
@@ -10,6 +11,12 @@ const userSchema = mongoose.Schema({
         type:String,
         required:true,
     }
+})
+
+userSchema.pre('save',async function (next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt)
+    next()
 })
 
 const User = new mongoose.model('user',userSchema);
